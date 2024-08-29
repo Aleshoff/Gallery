a5d124c70b55fddb1144931375b2cb9d97333be8
Olexandr Al oshyn <aleshoff@gmail.com>
2024-08-29 14:55:45 +0300

    Controlled Search Component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,21 @@
+import { useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from './components/Header';
+import Search from './components/Search';
+
 
 function App() {
+  const [word, setWord] = useState('');
+  
+  const handleSearchSubmit = (event) => {
+  event.preventDefault();
+  console.log(word);
+  };
+  //console.log(word);
   return (
     <div >
       <Header title="Photo Gallery"/>
+      <Search word={word} setWord={setWord} handleSubmit={handleSearchSubmit}/>
     </div>
   );
 }
