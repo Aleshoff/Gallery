7219e4cf26d730a9693ad8b719a4e3ec47ff62ef
Olexandr Al oshyn <aleshoff@gmail.com>
2024-08-30 10:03:32 +0300

    First API Call

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from './components/Header';
 import Search from './components/Search';
 
+const UNSPLASH_KEY = process.env.REACT_APP_UNSPLASH_KEY;
 
 function App() {
   const [word, setWord] = useState('');
@@ -10,8 +11,13 @@ function App() {
   const handleSearchSubmit = (event) => {
   event.preventDefault();
   console.log(word);
+  fetch(`https://api.unsplash.com/photos/random/?query=${word}&client_id=${UNSPLASH_KEY}`)
+  .then((response) => response.json())
+  .then((data) => console.log(data))
+  .catch((error) => console.log(error));
   };
   //console.log(word);
+  //console.log(UNSPLASH_KEY);
   return (
     <div >
       <Header title="Photo Gallery"/>
