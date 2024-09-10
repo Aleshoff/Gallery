8a8dde075dca9e12f16fa6ed3199750680172105
Olexandr Al oshyn <aleshoff@gmail.com>
2024-09-10 15:42:58 +0300

    Added Toast notifications

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,6 +7,8 @@ import ImageCard from "./components/ImageCard";
 import { Container, Row, Col } from "react-bootstrap";
 import Welcome from "./components/Welcome";
 import Spinner from "./components/Spinner";
+import { ToastContainer, toast } from "react-toastify";
+import 'react-toastify/dist/ReactToastify.css';
 
 //const UNSPLASH_KEY = process.env.REACT_APP_UNSPLASH_KEY;
 const API_URL = process.env.REACT_APP_API_URL || "http://127.0.0.1:5050";
@@ -23,8 +25,10 @@ function App() {
       const result = await axios.get(`${API_URL}/images`);
       setImages(result.data || []);
       setLoader(false);
+      toast.success("Saved Images Downloaded!");
     } catch (error) {
       console.log(error);
+      toast.error(error.message);
     }
   };
 
@@ -47,8 +51,10 @@ function App() {
     try {
       const result = await axios.get(`${API_URL}/new-photo?query=${word}`);
       setImages([{ ...result.data, title: word }, ...images]);
+      toast.info(`New Image ${word.toUpperCase()} was found!`);
     } catch (error) {
       console.log(error);
+      toast.error(error.message);
     }
     setWord("");
   };
@@ -62,10 +68,12 @@ function App() {
         imageToDelete
       );
       if (result.data?.deleted_id) {
+        toast.warn(`Image ${images.find((image) => image.id === id).title.toUpperCase()} was Deleted!`);
         setImages(images.filter((image) => image.id !== id));
       }
     } catch (error) {
       console.log(error);
+      toast.error(error.message);
     }
   };
 
@@ -81,9 +89,11 @@ function App() {
             image.id === id ? { ...image, saved: true } : image
           )
         );
+        toast.info(`Image ${imageToSave.title.toUpperCase()} was saved!`);
       }
     } catch (error) {
       console.log(error);
+      toast.error(error.message);
     }
   };
   //console.log(word);
@@ -92,28 +102,34 @@ function App() {
     <div>
       <Header title="Photo Gallery" />
       {loader ? (
-          <Spinner />
-        ) : (
+        <Spinner />
+      ) : (
         <>
-        <Search word={word} setWord={setWord} handleSubmit={handleSearchSubmit} />
-        <Container className="mt-4">
-          { images.length ? (
-            <Row xs={1} md={2} lg={3}>
-              {images.map((image, i) => (
-                <Col key={i} className="pb-3">
-                  <ImageCard
-                    image={image}
-                    deleteImage={handleDeleteImage}
-                    saveImage={handleSavedImage}
-                  />
-                </Col>
-              ))}
-            </Row>
-          ) : (
-            <Welcome />
-          )}
-        </Container></>)}
-      
+          <Search
+            word={word}
+            setWord={setWord}
+            handleSubmit={handleSearchSubmit}
+          />
+          <Container className="mt-4">
+            {images.length ? (
+              <Row xs={1} md={2} lg={3}>
+                {images.map((image, i) => (
+                  <Col key={i} className="pb-3">
+                    <ImageCard
+                      image={image}
+                      deleteImage={handleDeleteImage}
+                      saveImage={handleSavedImage}
+                    />
+                  </Col>
+                ))}
+              </Row>
+            ) : (
+              <Welcome />
+            )}
+          </Container>
+        </>
+      )}
+      <ToastContainer position="bottom-right" />
     </div>
   );
 }
