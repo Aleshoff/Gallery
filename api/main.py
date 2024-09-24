96cd0a5571d692a75c39e2028a0409e16ad0bee1
Aloshyn Olexandr <aleshoff@gmail.com>
2024-09-24 13:31:50 +0300

    Redirection changed2

--- api/main.py
+++ api/main.py
@@ -24,7 +24,7 @@ app.config["DEBUG"] = DEBUG
 
 @app.route("/")
 def initial():
-    return redirect("http://localhost:3000", code=307)
+    return redirect("http://alolprojectspace.com/front/", code=302)
 
 @app.route("/new-photo")
 def new_photo():
